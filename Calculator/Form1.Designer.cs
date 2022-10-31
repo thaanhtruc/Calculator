842c84dbb23cc8bf133475915bdfca41bba8a711
thaanhtruc <2054052077truc@ou.edu.vn>
2022-10-31 12:51:23 +0700

    Update Form

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -28,13 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbSoA = new System.Windows.Forms.Label();
+            this.lbSoB = new System.Windows.Forms.Label();
+            this.lbKQ = new System.Windows.Forms.Label();
+            this.txtSoA = new System.Windows.Forms.TextBox();
+            this.txtSoB = new System.Windows.Forms.TextBox();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbSoA
+            // 
+            this.lbSoA.AutoSize = true;
+            this.lbSoA.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSoA.Location = new System.Drawing.Point(55, 52);
+            this.lbSoA.Name = "lbSoA";
+            this.lbSoA.Size = new System.Drawing.Size(85, 31);
+            this.lbSoA.TabIndex = 0;
+            this.lbSoA.Text = "Số A:";
+            // 
+            // lbSoB
+            // 
+            this.lbSoB.AutoSize = true;
+            this.lbSoB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSoB.Location = new System.Drawing.Point(55, 130);
+            this.lbSoB.Name = "lbSoB";
+            this.lbSoB.Size = new System.Drawing.Size(85, 31);
+            this.lbSoB.TabIndex = 1;
+            this.lbSoB.Text = "Số B:";
+            // 
+            // lbKQ
+            // 
+            this.lbKQ.AutoSize = true;
+            this.lbKQ.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbKQ.Location = new System.Drawing.Point(55, 213);
+            this.lbKQ.Name = "lbKQ";
+            this.lbKQ.Size = new System.Drawing.Size(129, 31);
+            this.lbKQ.TabIndex = 2;
+            this.lbKQ.Text = "Kết Quả:";
+            // 
+            // txtSoA
+            // 
+            this.txtSoA.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSoA.Location = new System.Drawing.Point(230, 52);
+            this.txtSoA.Name = "txtSoA";
+            this.txtSoA.Size = new System.Drawing.Size(237, 38);
+            this.txtSoA.TabIndex = 3;
+            // 
+            // txtSoB
+            // 
+            this.txtSoB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSoB.Location = new System.Drawing.Point(230, 130);
+            this.txtSoB.Name = "txtSoB";
+            this.txtSoB.Size = new System.Drawing.Size(237, 38);
+            this.txtSoB.TabIndex = 4;
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKQ.Location = new System.Drawing.Point(230, 210);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(237, 38);
+            this.txtKQ.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.txtSoB);
+            this.Controls.Add(this.txtSoA);
+            this.Controls.Add(this.lbKQ);
+            this.Controls.Add(this.lbSoB);
+            this.Controls.Add(this.lbSoA);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbSoA;
+        private System.Windows.Forms.Label lbSoB;
+        private System.Windows.Forms.Label lbKQ;
+        private System.Windows.Forms.TextBox txtSoA;
+        private System.Windows.Forms.TextBox txtSoB;
+        private System.Windows.Forms.TextBox txtKQ;
     }
 }
 
